0e83bba7447f0eda011e6bc36f13e3713a14aed9
yanshen02 <yanshen02@meituan.com>
2022-05-31 16:52:59 +0800

    2

--- codewars/src/kata_train.md
+++ codewars/src/kata_train.md
@@ -6,3 +6,15 @@ We need a function that can transform a string into a number. What ways of achie
 
 Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
+
+# Train 2: Find the smallest integer in the array
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+
+Given [34, -345, -1, 100] your solution will return -345
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
--- codewars/src/solution.rs
+++ codewars/src/solution.rs
@@ -1,7 +1,13 @@
-fn main(){
+fn main() {
     // Test 1
     let num = string_to_number("12345");
-    assert_eq!(num , 12345);
+    assert_eq!(num, 12345);
+
+    // Test 2
+    let arr1 = [1, 1, 2, 3, 4, 5, 6, 7];
+    assert_eq!(find_smallest_int(&arr1), 1);
+    let arr2 = [-1,-55,2,32,0];
+    assert_eq!(find_smallest_int(&arr2), -55);
 }
 
 // Train 1: Convert a String to a Number!
@@ -9,3 +15,7 @@ fn string_to_number(s: &str) -> i32 {
     s.parse().unwrap()
 }
 
+// Train 2: Find the smallest integer in the array
+fn find_smallest_int(arr: &[i32]) -> i32 {
+    *arr.iter().min().unwrap()
+}
