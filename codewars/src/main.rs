2bdd08e6000056d98f8acea714b98c2235adf8f3
yanshen02 <yanshen02@meituan.com>
2022-06-01 15:56:19 +0800

    change structure

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -1,4 +1,20 @@
+mod solution;
+use crate::solution::*;
 fn main() {
-    println!("Hello, world!");
-}
+    // Test 1
+    let num = string_to_number("12345");
+    assert_eq!(num, 12345);
 
+    // Test 2
+    let arr1 = [1, 1, 2, 3, 4, 5, 6, 7];
+    assert_eq!(find_smallest_int(&arr1), 1);
+    let arr2 = [-1, -55, 2, 32, 0];
+    assert_eq!(find_smallest_int(&arr2), -55);
+
+    // Test 3
+    let dna1 = String::from("GGCCAATTTTTGCTACGATCGATCGCTATCACGATCTACA");
+    assert_eq!(
+        dna_to_rna(&dna1),
+        "GGCCAAUUUUUGCUACGAUCGAUCGCUAUCACGAUCUACA"
+    )
+}
--- codewars/src/solution.rs
+++ codewars/src/solution.rs
@@ -1,30 +1,16 @@
-fn main() {
-    // Test 1
-    let num = string_to_number("12345");
-    assert_eq!(num, 12345);
-
-    // Test 2
-    let arr1 = [1, 1, 2, 3, 4, 5, 6, 7];
-    assert_eq!(find_smallest_int(&arr1), 1);
-    let arr2 = [-1,-55,2,32,0];
-    assert_eq!(find_smallest_int(&arr2), -55);
-
-    // Test 3
-    let dna1 = String::from("GGCCAATTTTTGCTACGATCGATCGCTATCACGATCTACA");
-    assert_eq!(dna_to_rna(&dna1) , "GGCCAAUUUUUGCUACGAUCGAUCGCUAUCACGAUCUACA")
-}
-
 // Train 1: Convert a String to a Number!
-fn string_to_number(s: &str) -> i32 {
+pub fn string_to_number(s: &str) -> i32 {
     s.parse().unwrap()
 }
 
 // Train 2: Find the smallest integer in the array
-fn find_smallest_int(arr: &[i32]) -> i32 {
+pub fn find_smallest_int(arr: &[i32]) -> i32 {
     *arr.iter().min().unwrap()
 }
 
 // Train 3: DNA to RNA Conversion
-fn dna_to_rna(dna: &str) -> String {
+pub fn dna_to_rna(dna: &str) -> String {
     dna.replace("T", "U")
 }
+
+// Train 4: 
