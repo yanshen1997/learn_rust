7ac17c4328cf7f46535368b5846654ff9738786b
yanshen1997 <yans@mail.ustc.edu.cn>
2022-06-03 10:45:41 +0800

    添加单元测试

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -1,21 +1,5 @@
 mod solution;
 mod leetcode;
-use crate::solution::*;
 fn main() {
-    // Test 1
-    let num = string_to_number("12345");
-    assert_eq!(num, 12345);
 
-    // Test 2
-    let arr1 = [1, 1, 2, 3, 4, 5, 6, 7];
-    assert_eq!(find_smallest_int(&arr1), 1);
-    let arr2 = [-1, -55, 2, 32, 0];
-    assert_eq!(find_smallest_int(&arr2), -55);
-
-    // Test 3
-    let dna1 = String::from("GGCCAATTTTTGCTACGATCGATCGCTATCACGATCTACA");
-    assert_eq!(
-        dna_to_rna(&dna1),
-        "GGCCAAUUUUUGCUACGAUCGAUCGCUAUCACGAUCUACA"
-    )
 }
--- codewars/src/solution.rs
+++ codewars/src/solution.rs
@@ -13,4 +13,37 @@ pub fn dna_to_rna(dna: &str) -> String {
     dna.replace("T", "U")
 }
 
-// Train 4: 
+// Train 4:
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Test 1
+    #[test]
+    fn test_string_to_number() {
+        let num = string_to_number("12345");
+        assert_eq!(num, 12345);
+    }
+
+    // Test 2
+    #[test]
+    fn test_find_smallest_int() {
+        let arr1 = [1, 1, 2, 3, 4, 5, 6, 7];
+        assert_eq!(find_smallest_int(&arr1), 1);
+        let arr2 = [-1, -55, 2, 32, 0];
+        assert_eq!(find_smallest_int(&arr2), -55);
+    }
+
+    // Test 3
+    #[test]
+    fn test_dna_to_rna() {
+        let dna1 = String::from("GGCCAATTTTTGCTACGATCGATCGCTATCACGATCTACA");
+        assert_eq!(
+            dna_to_rna(&dna1),
+            "GGCCAAUUUUUGCUACGAUCGAUCGCUAUCACGAUCUACA"
+        )
+    }
+}
