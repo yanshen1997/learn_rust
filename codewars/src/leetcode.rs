8fb5e09c2f4ddafcd1c9453d21901da87cbfa409
yanshen1997 <yans@mail.ustc.edu.cn>
2022-07-03 10:08:31 +0800

    leetcode 556

--- codewars/src/leetcode.rs
+++ codewars/src/leetcode.rs
@@ -1,13 +1,45 @@
-use std::{cmp::Ordering, vec,cmp::min};
+use std::{cmp::min, cmp::Ordering, collections::HashMap, vec, i32::MAX};
 // 测试在leetcode上完成
 struct Solution;
 impl Solution {
+    //556. 下一个更大元素 III
+    pub fn next_greater_element(n: i32) -> i32 {
+        let mut mem_digits = vec![];
+        let mut digits_map = HashMap::<i32, Vec<i32>>::new();
+        let mut index = 0;
+        let mut nn:i64 = n as i64;
+        while nn > 0 {
+            let curr_digit = (nn % 10) as i32;
+            for v in curr_digit + 1..=9 {
+                if digits_map.contains_key(&v) {
+                    let latest_idx = digits_map.get(&v).unwrap().last().unwrap();
+                    mem_digits[*latest_idx as usize] = curr_digit;
+                    nn -= curr_digit as i64;
+                    nn += v as i64;
+                    mem_digits.sort();
+                    mem_digits.iter().for_each(|&curr| {
+                        nn *= 10;
+                        nn += curr as i64;
+                    });
+                    return if nn > i32::MAX as i64 { -1 } else { nn as i32};
+                }
+            }
+            mem_digits.push(curr_digit);
+            digits_map
+                .entry(curr_digit)
+                .or_insert_with(|| Vec::new())
+                .push(index);
+            index += 1;
+            nn /= 10;
+        }
+        -1
+    }
 
     //剑指 Offer II 091. 粉刷房子
     pub fn min_cost(costs: Vec<Vec<i32>>) -> i32 {
-        let mut dp = vec![0;3];
-        let mut dp_next = vec![0;3];
-        let change_val = |dp1: &mut Vec<i32>,dp2: &Vec<i32>|{
+        let mut dp = vec![0; 3];
+        let mut dp_next = vec![0; 3];
+        let change_val = |dp1: &mut Vec<i32>, dp2: &Vec<i32>| {
             for i in 0..dp2.len() {
                 dp1[i] = dp2[i];
             }
@@ -16,9 +48,9 @@ impl Solution {
             dp_next[0] = v[0] + min(dp[1], dp[2]);
             dp_next[1] = v[1] + min(dp[0], dp[2]);
             dp_next[2] = v[2] + min(dp[1], dp[0]);
-            change_val(&mut dp , &dp_next);
+            change_val(&mut dp, &dp_next);
         });
-        return min(min(dp[0],dp[1]), dp[2])
+        return min(min(dp[0], dp[1]), dp[2]);
     }
 
     //剑指 Offer II 008. 和大于等于 target 的最短子数组
